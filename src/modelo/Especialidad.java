e74a1c55c6e285f4e5aa7919429a9360e0bc421b
Fernando Cabrera <feercabrera429@gmail.com>
2023-10-09 17:35:32 -0300

    Se agrego clase especialidad

---
+++ src/modelo/Especialidad.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Especialidad {
+	
+	private String especialidad;
+
+	public Especialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+}
