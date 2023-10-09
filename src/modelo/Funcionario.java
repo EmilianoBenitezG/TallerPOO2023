50ee7901b8e7d03b0aaaa8c839a5295484cbe343
Fernando Cabrera <feercabrera429@gmail.com>
2023-10-09 17:33:53 -0300

    Se agrego clase funcionario y medico

---
+++ src/modelo/Funcionario.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Funcionario extends Persona{
+
+	public Funcionario() {
+	}
+	
+}
---
+++ src/modelo/Medico.java
@@ -0,0 +1,38 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Medico extends Funcionario{
+	
+	private String matricula;
+	private List<DetallesEspecialidad> especialidades;
+	 
+	public Medico() {
+		especialidades = new ArrayList<>();
+	}
+	
+	public Medico(String matricula) {
+		this.matricula = matricula;
+		especialidades = new ArrayList<>();
+	}
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	public List<DetallesEspecialidad> getEspecialidades() {
+        return especialidades;
+    }
+	
+	// Método para agregar una especialidad
+    public void agregarEspecialidad(DetallesEspecialidad especialidad) {
+        especialidades.add(especialidad);
+    }
+    
+}
