86107631c453cabb4c22a5111788e1248b126259
Benitez  Emiliano <benitezemiliano2096@gmail.com>
2023-10-16 21:26:28 -0300

    Agrego GestorHospital

---
+++ src/modelo/GestorHospital.java
@@ -0,0 +1,43 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class GestorHospital {
+	
+	//por definirse
+	public ArrayList<String> cantPacFechas(int ID) {
+		 ArrayList<String> cantidad = null;
+		return cantidad;
+	}
+	
+	//por definirse
+	public  ArrayList<String> cantPacFechasEdades(int ID) {
+		 ArrayList<String> cantidad = null;
+		return cantidad;
+	}
+	
+	//por definirse
+	public  ArrayList<String> consultaPacFechas(int ID) {
+		 ArrayList<String> cantidad = null;
+		return cantidad;
+	}
+	
+	//por definirse
+	public  ArrayList<String> medicoAtendioPacFechas(int ID) {
+		 ArrayList<String> cantidad = null;
+		return cantidad;
+	}
+	
+	//por definirse
+	public  ArrayList<String> triageFechasConColores(int ID) {
+		ArrayList<String> cantidad = null;
+		return cantidad;
+	}
+	
+	//por definirse
+	public  ArrayList<String> cantTriageConColorPropSistColorAsignadoFunc(int ID) {
+		ArrayList<String> cantidad = null;
+		return cantidad;
+	}
+
+}
